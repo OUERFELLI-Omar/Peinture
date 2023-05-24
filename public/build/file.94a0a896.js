6f81ea39ec77c04d7719ba0e460b1af7806a8726
OUERFELLI Omar <omarouerfelli10@gmail.com>
2023-05-24 22:43:00 +0100

    mn dashboard w enty mechi

---
+++ public/build/file.94a0a896.js
@@ -0,0 +1,1 @@
+(self.webpackChunk=self.webpackChunk||[]).push([[745],{6563:(a,e,c)=>{var n=c(9755);n(".flag").on("click",(function(){var a=n(this).attr("value");n.ajax({type:"POST",url:"changeLocale",data:{langue:a},success:function(a){location.reload()}})}))}},a=>{a.O(0,[755],(()=>{return e=6563,a(a.s=e);var e}));a.O()}]);
